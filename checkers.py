91d6e20246c97ce90affb284b8d5f3f6d20907e3
Marcos Guilherme Thomaz da Silva <83443828+guithomazs@users.noreply.github.com>
2023-08-17 17:43:45 -0500

    added removing piece functionality

--- checkers.py
+++ checkers.py
@@ -33,6 +33,8 @@ class Checkers:
         self.player_one_pieces = []
         self.player_two_pieces = []
         self.obrigatory_movements = {}
+        self.can_change_first_click = True
+        self.buttons = [[None for i in range(8)] for j in range(8)]
         self.createHeader()
         self.createStartBoard()
 
@@ -136,66 +138,107 @@ class Checkers:
                         )
         lbl.bind('<ButtonPress-1>', lambda event, lbl=lbl, row=row, column=column: self.checkMovement(lbl, row, column))
 
+        self.buttons[row][column] = lbl
+
         lbl.grid(row=row, column=column)
 
     def checkMovement(self, clicked_label:Label, row, column):
         # define the element to be moved
-        if len(self.obrigatory_movements) and (row, column) not in self.obrigatory_movements:
-            return
         if not self.first_click:
+            if len(self.obrigatory_movements) and (row, column) not in self.obrigatory_movements.keys():
+                print(self.obrigatory_movements)
+                return
             if not (self.game_table[row][column] == self.player_turn):
                 return
             clicked_label.config(bg='lime')
             self.first_click = clicked_label
             return
+        
         # if it isn't the first click, and is the same label of the first click, reset the click
         if self.first_click == clicked_label:
+            if not self.can_change_first_click:
+                return
             clicked_label.config(bg='white')
             self.first_click = None
             return
+        
         # if the house it's trying to move it's occupied then it isn't able to move
         if self.game_table[row][column] is not None:
-            print('será?')
             return
         
-        print('aabbcc', [row, column])
         first_click_row = self.first_click.grid_info()['row']
         first_click_col = self.first_click.grid_info()['column']
-        adjacent_houses = [
+
+        if (
+            len(self.obrigatory_movements) and 
+                [row, column] not in 
+                    list(self.obrigatory_movements[(first_click_row, first_click_col)])
+            ):
+            return
+        
+        # player 2 can only move downwards and player 1 can only move upwards if 
+        # it's not for capture an enemy piece
+        possible_moves = [
             [first_click_row-1, first_click_col-1],
             [first_click_row-1, first_click_col+1], 
+        ] if self.player_turn else [
             [first_click_row+1, first_click_col-1],
             [first_click_row+1, first_click_col+1]
-        ]
-        if not len(self.obrigatory_movements) and [row, column] not in adjacent_houses:
-            print('foi aqui#1')
+            ]
+        if not len(self.obrigatory_movements) and [row, column] not in possible_moves:
             return
         if len(self.obrigatory_movements):
-            print('entrar entrou')
-            if[row, column] not in self.obrigatory_movements[(row, column)]:
-                print([row, column])
+            if[row, column] not in self.obrigatory_movements[(first_click_row, first_click_col)]:
                 return
-            print('AHORA FOI CARALHO.', [row, column], [row,column] in self.obrigatory_movements[(row, column)])
-        print('entrou nada não')
+            print('AHORA FOI CARALHO.', [row, column], [row,column] in self.obrigatory_movements[(first_click_row, first_click_col)])
+            self.remove_piece(first_click_row, first_click_col, row, column)
         # self.checkNext(first_click_row, first_click_col, row, column)
 
-        self.game_table[first_click_row][first_click_col] = None
-        self.game_table[row][column] = self.player_turn
+        self.changePiecePosition(first_click_row, first_click_col, row, column, clicked_label)    
 
-        if self.player_turn:
-            self.player_one_pieces.pop(self.player_one_pieces.index([first_click_row, first_click_col]))
-            self.player_one_pieces.append([row, column])
-        else:
-            self.player_two_pieces.pop(self.player_two_pieces.index([first_click_row, first_click_col]))
-            self.player_two_pieces.append([row, column])
-
-        self.first_click.config(text='')
-        clicked_label.config(text=self.get_tile_text(row, column), 
-                             fg=self.get_tile_foreground(row, column))
-        self.first_click.config(bg='white')
-        self.first_click = None
+        self.mapPiece(row, column)
 
         self.changePlayerTurn()    
+
+    def remove_piece(self, first_row, first_column, last_row, last_column):
+        diff_row = (first_row - last_row) // 2
+        diff_column = (first_column - last_column) // 2
+        wanted_row = first_row -  diff_row
+        wanted_column = first_column - diff_column
+        self.buttons[wanted_row][wanted_column].config(text='')
+        self.game_table[wanted_row][wanted_column] = None
+
+        (
+            self.player_one_pieces.pop(self.player_one_pieces.index([wanted_row, wanted_column])) 
+            if  not self.player_turn 
+                else 
+                    self.player_two_pieces.pop(self.player_two_pieces.index([wanted_row, wanted_column]))
+        )
+
+    def changePiecePosition(self, first_row, first_col, actual_row, actual_col, clicked_label):
+        self.game_table[first_row][first_col] = None
+        self.game_table[actual_row][actual_col] = self.player_turn
+        
+        if self.player_turn:
+            self.player_one_pieces.pop(self.player_one_pieces.index([first_row, first_col]))
+            self.player_one_pieces.append([actual_row, actual_col])
+        else:
+            self.player_two_pieces.pop(self.player_two_pieces.index([first_row, first_col]))
+            self.player_two_pieces.append([actual_row, actual_col])
+
+        self.first_click.config(text='')
+        clicked_label.config(text=self.get_tile_text(actual_row, actual_col), 
+                             fg=self.get_tile_foreground(actual_row, actual_col))
+        self.first_click.config(bg='white')
+        self.first_click = None
+    
+    def mapPiece(self, piece_row, piece_column):
+        adjacent_houses = [
+            [piece_row-1, piece_column-1],
+            [piece_row-1, piece_column+1], 
+            [piece_row+1, piece_column-1],
+            [piece_row+1, piece_column+1]
+        ]
 
     def changePlayerTurn(self):
         self.player_turn = not self.player_turn
@@ -214,7 +257,6 @@ class Checkers:
             for adjacent in adjacent_houses:
                 if self.validaCasa(adjacent):
                     if self.game_table[adjacent[0]][adjacent[1]] == (not self.player_turn):
-                        print('not fucking')
                         self.checkNext(piece[0], piece[1], adjacent[0], adjacent[1])
 
     def validaCasa(self, casa):
@@ -227,15 +269,22 @@ class Checkers:
         diff_col = piece_col - next_col
         next_row = next_row - diff_row
         next_col = next_col - diff_col
+
+        # this line verify if the next element is not out of the table
         if (next_row < 0 or next_row >= len(self.game_table)) or next_col < 0 or next_col >= len(self.game_table):
-            print('out of table')
             return False
+        
+        # this line verify if the next house of the direction is empty or fullfiled
         if self.game_table[next_row][next_col] is not None:
-            print("TEM ESPAÇO NA NEXT N")
             return False
-        self.obrigatory_movements[piece_row, piece_col] = [next_row, next_col]
-        print('TEM ESPAÇO NA NEXT', [piece_row, piece_col], [next_row, next_col])
+        self.add_obrigatory_movement_option(piece_row, piece_col, next_row, next_col)
         return True
+    
+    def add_obrigatory_movement_option(self, key_row, key_column, value_row, value_column):
+        if (key_row, key_column) not in self.obrigatory_movements.keys():
+            # creating the options of movements when is 
+            self.obrigatory_movements[key_row, key_column] = []
+        self.obrigatory_movements[key_row, key_column].append([value_row, value_column])
         
     def get_tile_color(self, row, column):
         return (EVEN_TILES if (is_odd(row) and not is_odd(column)) 
